e9457d808ff4abc271e8c7a2c6bb6a4154f9f92b
Bernava <guilhermebernava00@gmail.com>
2022-03-02 12:00:25 -0300

    Funcionando Basico, com o firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppExample from './examples/AppExample';
+import App from './App';
 
 import './services/firebase';
 
-//vai pegar todos os componentes que estiverem dentro do React.StrictMode e vai injetar na div com id root
 ReactDOM.render(
-  //Aqui sera onde vai colocar todos os componentes(telas) que forem criadas
   <React.StrictMode>    
-    <AppExample />
+    <App />
   </React.StrictMode>,
-  //esse e como o REACT vai encontrar aquele Id e injetar dentro dele
   document.getElementById('root')
 );
